d2411023eec461c29390cd4ae4320c32a47312db
A.Aydogan Asus Desktop <aydoganab@itu.edu.tr>
2021-05-14 16:21:50 +0300

    development continued

--- src/js/kim101-sonuc.js
+++ src/js/kim101-sonuc.js
@@ -9,144 +9,80 @@ xhr.onload = function () {
     let r = JSON.parse(xhr.responseText);
 
     //student info
-    let s = r.student;
-    document.getElementById("sno").innerHTML = s.no;
-    document.getElementById("sname").innerHTML = s.adsoyad;
-    document.getElementById("sdil").innerHTML = s.dil;
-    document.getElementById("scrn").innerHTML = s.crn;
+    document.getElementById("sno").innerHTML = r.std.no;
+    document.getElementById("sname").innerHTML = r.std.adsoyad;
+    document.getElementById("sdil").innerHTML = r.std.dil;
+    document.getElementById("scrn").innerHTML = r.std.crn;
 
-    let ouy = document.getElementById("souy");
-    let img = document.createElement("img");
-    img.setAttribute("src", "###async_server###/userpics/t_" + s.resim);
-    img.setAttribute("alt", s.ouyname);
-    img.style.maxHeight = "25px";
-    img.classList.add("img-fluid", "me-2", "rounded-circle");
-    ouy.append(img);
-    ouy.append(document.createTextNode(s.ouyname));
-    let email = document.createElement("a");
-    email.setAttribute("href", "mailto:" + s.eposta);
-    email.classList.add("ms-auto", "card-link");
-    email.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1.3em" viewBox="0 0 24 24" width="1.3em" fill="currentColor"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6zm-2 0l-8 5-8-5h16zm0 12H4V8l8 5 8-5v10z"/></svg>';
-    ouy.append(email);
+    //öğretim üyesi
+    if (Object.keys(r.ouy).length > 0){
+        let souy = document.getElementById("souy");
+        let img = document.createElement("img");
+        img.setAttribute("src", "###async_server###/userpics/t_" + r.ouy.resim);
+        img.setAttribute("alt", r.ouy.name);
+        img.style.maxHeight = "25px";
+        img.classList.add("img-fluid", "me-2", "rounded-circle");
+        souy.append(img);
+        souy.append(document.createTextNode(r.ouy.name));
+        let email = document.createElement("a");
+        email.setAttribute("href", "mailto:" + r.ouy.eposta);
+        email.classList.add("ms-auto", "card-link");
+        email.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1.3em" viewBox="0 0 24 24" width="1.3em" fill="currentColor"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6zm-2 0l-8 5-8-5h16zm0 12H4V8l8 5 8-5v10z"/></svg>';
+        souy.append(email);
+    }
+
 
     //sınav yeri bilgileri
-    if (s.sinav_yeri != "") {
+    if (Object.keys(r.sinav_yeri).length > 0) {
 
         document.getElementById("locdonem").innerHTML = r.donem_tr;
-        document.getElementById("locsy").innerHTML = s.sinav_yeri;
-        document.getElementById("locgr").innerHTML = s.grup;
-        document.getElementById("locsn").innerHTML = s.derslik_sira_no;
+        document.getElementById("locsy").innerHTML = r.sinav_yeri.derslik;
+        document.getElementById("locgr").innerHTML = r.sinav_yeri.grup;
+        document.getElementById("locsn").innerHTML = r.sinav_yeri.sira_no;
 
         document.getElementById("loc").classList.remove("d-none");
     }
 
-    let osnCon = document.getElementById("osn-con");
+    //sinav notları
+    if (r.sinavlar.length > 0) {
 
-    //ödev notları
-    if (r.sd.odev_sayisi > 0) {
+        let tbnot = document.getElementById("tbnot");
 
-        for (let i = 1; i <= r.sd.odev_sayisi; i++) {
+        for (let s = 0; s < r.sinavlar.length; s++) {
+            let sitem = r.sinavlar[s];
 
-            let div1 = document.createElement("div");
-            div1.classList.add("row", "align-items-center", "mb-3", "gx-3");
+            let str = document.createElement("tr");
 
-            let div2 = document.createElement("div");
-            div2.classList.add("col", "fw-bold");
-            div2.append(document.createTextNode("Ödev " + i));
+            let sth = document.createElement("th");
+            sth.setAttribute("scope", "col");
+            sth.innerHTML = sitem.sinav;
 
-            let div3 = document.createElement("div");
-            div3.classList.add("col");
-            div3.innerHTML = eval("s.o" + i);
+            let snotu = document.createElement("td");
+            snotu.innerHTML=sitem.notu;
 
-            let div3_2 = document.createElement("div");
-            div3_2.classList.add("col");
+            let sgort = document.createElement("td");
+            sgort.innerHTML=sitem.gort;
 
-            let div4 = document.createElement("div");
-            div4.classList.add("col-auto");
-
+            let sdetay = document.createElement("td");
+            sdetay.classList.add("text-end");
             let btn = document.createElement("button");
             btn.setAttribute("type", "button");
-            btn.setAttribute("data-s", "o" + i);
-            btn.setAttribute("title", "Ödev " + i + " detayları");
-            btn.classList.add("btn", "btn-xs", "btn-outline-secondary", "rounded-pill", "btn-odev-detay");
-            btn.innerHTML = "Detaylar";
-            div4.append(btn);
+            btn.setAttribute("data-s", sitem.turu);
+            btn.classList.add("btn", "btn-sm", "btn-outline-secondary");
+            btn.append(document.createTextNode("Detaylar"));
+            sdetay.append(btn);
 
-            div1.append(div2, div3, div3_2, div4);
-            osnCon.append(div1);
+            str.append(sth, snotu, sgort, sdetay);
 
+            tbnot.append(str);
         }
 
+        //show sinavlar card
+        document.getElementById("osnCon").classList.remove("d-none");
     }
 
-    //arasınav notları
-    if (r.sd.as_sayisi > 0) {
 
-        for (let i = 1; i <= r.sd.as_sayisi; i++) {
 
-            let div1 = document.createElement("div");
-            div1.classList.add("row", "align-items-center", "mb-3", "gx-3");
-
-            let div2 = document.createElement("div");
-            div2.classList.add("col", "fw-bold");
-            div2.append(document.createTextNode("Arasınav " + i));
-
-            let div3 = document.createElement("div");
-            div3.classList.add("col");
-            div3.innerHTML = eval("s.as" + i + "s1");
-
-            let div3_2 = document.createElement("div");
-            div3_2.classList.add("col");
-
-            let div4 = document.createElement("div");
-            div4.classList.add("col-auto");
-
-            let btn = document.createElement("button");
-            btn.setAttribute("type", "button");
-            btn.setAttribute("data-s", "as" + i);
-            btn.setAttribute("title", "Ara sınav " + i + " detayları");
-            btn.classList.add("btn", "btn-xs", "btn-outline-secondary", "rounded-pill", "btn-odev-detay");
-            btn.innerHTML = "Detaylar";
-            div4.append(btn);
-
-            div1.append(div2, div3, div3_2, div4);
-            osnCon.append(div1);
-
-        }
-
-    }
-
-    //final sınavı notu
-    let div1 = document.createElement("div");
-    div1.classList.add("row", "align-items-center", "mb-3", "gx-3");
-
-    let div2 = document.createElement("div");
-    div2.classList.add("col", "fw-bold");
-    div2.append(document.createTextNode("Final sınavı "));
-
-    let div3 = document.createElement("div");
-    div3.classList.add("col");
-    div3.innerHTML = s.fss1;
-
-    let div3_2 = document.createElement("div");
-    div3_2.classList.add("col");
-
-    let div4 = document.createElement("div");
-    div4.classList.add("col-auto");
-
-    let btn = document.createElement("button");
-    btn.setAttribute("type", "button");
-    btn.setAttribute("data-s", "fs");
-    btn.setAttribute("title", "Final sınavı detayları");
-    btn.classList.add("btn", "btn-xs", "btn-outline-secondary", "rounded-pill", "btn-odev-detay");
-    btn.innerHTML = "Detaylar";
-    div4.append(btn);
-
-    div1.append(div2, div3, div3_2, div4);
-    osnCon.append(div1);
-
-    //show sinavlar card
-    document.getElementById("osn").classList.remove("d-none");
 
     //detaylar on click
     let db = document.querySelectorAll(".btn-odev-detay");
