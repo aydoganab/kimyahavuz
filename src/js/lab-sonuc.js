e4c501d50e373cbaafba6cec051513475bc603db
A.Aydogan Dell Laptop <aaDL@itu.edu.tr>
2021-05-17 01:46:57 +0300

    development continued

--- src/js/lab-sonuc.js
+++ src/js/lab-sonuc.js
@@ -1,8 +1,8 @@
-function ge(id) {
+function kh_getElement(id) {
     return document.getElementById(id);
 }
 
-function ce(elem){
+function kh_createElement(elem){
     return document.createElement(elem);
 }
 
@@ -18,78 +18,78 @@ xhr.onload = function () {
     if (responseObject.s == 2) {
 
         //öğrenci
-        ge("siNo").innerHTML = responseObject.std.no;
-        ge("siName").innerHTML = responseObject.std.name;
-        ge("siCrn").innerHTML = responseObject.std.crn;
-        let siImg=ge("siImg");
+        kh_getElement("siNo").innerHTML = responseObject.std.no;
+        kh_getElement("siName").innerHTML = responseObject.std.name;
+        kh_getElement("siCrn").innerHTML = responseObject.std.crn;
+        let siImg=kh_getElement("siImg");
         siImg.setAttribute("src","###async_server###/userpics/"+responseObject.std.facimg);
         siImg.setAttribute("alt", responseObject.std.facname);
-        ge("siOuy").innerHTML=responseObject.std.facname;
-        ge("siEmail").setAttribute("href", "mailto:" + responseObject.std.facemail);
+        kh_getElement("siOuy").innerHTML=responseObject.std.facname;
+        kh_getElement("siEmail").setAttribute("href", "mailto:" + responseObject.std.facemail);
 
         //lab
-        ge("liLab").innerHTML = responseObject.std.lab;
-        ge("liDay").innerHTML = responseObject.std.gun;
-        ge("liHours").innerHTML = responseObject.std.saat;
+        kh_getElement("liLab").innerHTML = responseObject.std.lab;
+        kh_getElement("liDay").innerHTML = responseObject.std.gun;
+        kh_getElement("liHours").innerHTML = responseObject.std.saat;
 
         //experiment and grades
         Array.from(responseObject.cal).forEach(calItem => {
-            let tr = ce("tr");
+            let tr = kh_createElement("tr");
 
-            let td1 = ce("th");
+            let td1 = kh_createElement("th");
             td1.setAttribute("scope", "col");
             td1.className = "text-center";
             td1.innerHTML = calItem.week;
-            let td2 = ce("td");
+            let td2 = kh_createElement("td");
             td2.innerHTML = calItem.date;
-            let td3 = ce("td");
+            let td3 = kh_createElement("td");
             td3.className = "text-center";
             td3.innerHTML = calItem.expno;
-            let td4 = ce("td");
+            let td4 = kh_createElement("td");
             td4.innerHTML = calItem.exp;
-            let td5 = ce("td");
+            let td5 = kh_createElement("td");
             td5.className = "text-center";
             td5.innerHTML = calItem.expgrade;
 
             tr.append(td1, td2, td3, td4, td5);
 
-            ge("egTB").append(tr);
+            kh_getElement("egTB").append(tr);
         });
 
         //safety quiz
-        let trq = ce("tr");
-        let thq = ce("th");
+        let trq = kh_createElement("tr");
+        let thq = kh_createElement("th");
         thq.setAttribute("colspan", "4");
         thq.classList.add("text-end");
         thq.innerHTML = "Safety Quiz";
-        let tdq=ce("td");
+        let tdq=kh_createElement("td");
         tdq.className = "text-center";
         tdq.innerHTML=responseObject.std.quiz;
         trq.append(thq, tdq);
-        ge("egTB").append(trq);
+        kh_getElement("egTB").append(trq);
 
         //asistanlar
         Array.from(responseObject.ast).forEach(ast => {
 
-            let td1 = ce("td");
+            let td1 = kh_createElement("td");
             td1.innerHTML=ast.expno;
 
-            let td2 = ce("td");
+            let td2 = kh_createElement("td");
             td2.innerHTML=ast.name;
 
-            let td3 = ce("td");
+            let td3 = kh_createElement("td");
 
-            let eposta = ce("a");
+            let eposta = kh_createElement("a");
             eposta.setAttribute("href", "mailto:" + ast.eposta);
             eposta.innerHTML = "Eposta";
 
 
             td3.append(eposta);
 
-            let tr = ce("tr");
+            let tr = kh_createElement("tr");
             tr.append(td1, td2, td3);
 
-            ge("asTB").append(tr);
+            kh_getElement("asTB").append(tr);
         });
 
     }
